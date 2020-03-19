b9980ffe1810d6b5f60902666c7664b7877cada5
erick <grillo.erick1@gmail.com>
2020-03-18 22:02:52 -0600

    responsive producto

--- producto.php
+++ producto.php
@@ -214,7 +214,7 @@
             </div>
           </div>
         </footer>
-
+ 
       <!--JavaScript at end of body for optimized loading-->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
